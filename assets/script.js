c6b43da52f6d3bb37280346e71b86987338d2baf
solanadinero <computerdinero@gmail.com>
2025-10-27 13:04:12 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ function copyMint() {
 fetch('../data/price.json')
   .then(res => res.json())
   .then(data => {
- document.getElementById("liquidity").textContent = `Current Liquidity: $ ${Number(data.liquidity_usd).toFixed(2)}`;
+ document.getElementById("liquidity").textContent = `Current Liquidity: $${Number(data.liquidity_usd).toFixed(2)} USD`;
   })
   .catch(err => {
     console.error('Error loading price data:', err);
